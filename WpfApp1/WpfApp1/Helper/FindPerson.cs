50a79c340f39add70b7050cd3679443a72d47fb0
Bobrenko <94550307+Bobrenko@users.noreply.github.com>
2021-11-17 18:43:08 +0300

    Add files via upload
    Общие лабораторные для всех, с 1 по 4.

---
+++ WpfApp1/WpfApp1/Helper/FindPerson.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfApplDemo2018.Model;
+
+namespace WpfApp1.Helper
+{
+    public class FindPerson
+    {
+        int id;
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+        public bool PersonPredicate(Person per)
+        {
+            return per.Id == id;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Helper/FindRole.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WpfApplDemo2018.Model;
+namespace WpfApplDemo2018.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
